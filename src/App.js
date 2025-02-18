c4923dc39357b109b05159ae3294e410dc9885e7
ashish-me-15 <ashish.sharma75.email@gmail.com>
2025-02-18 23:41:16 +0530

    Added Routing and atyling to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
-import Home from "./Pages/Home"
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Favorites from "./Pages/Favorites"
+import Navbar from "./Component/Navbar"
+import "./css/App.css"
+
 function App() {
 
   return (
     <>
 
-    <Home />
+    <div>
+      <Navbar />
+
+    <main className='main=content'>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path="/favorites" element={<Favorites />} />
+    </Routes>
+   </main>
+    </div>
+
+   
     </>
   )
 }
